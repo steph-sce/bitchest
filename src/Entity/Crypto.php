fcf10816a63174c555a8af78794cc7911313076c
steph28 <stephane.soucheyre@gmail.com>
2019-02-05 16:27:46 +0100

    ADD admin and user controller

---
+++ src/Entity/Crypto.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CryptoRepository")
+ */
+class Crypto
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $logo;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $sigle;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(string $logo): self
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    public function getSigle(): ?string
+    {
+        return $this->sigle;
+    }
+
+    public function setSigle(string $sigle): self
+    {
+        $this->sigle = $sigle;
+
+        return $this;
+    }
+}
